4f6cd883491d7de6f940dd3e1b90965889218217
Kim-Yukyung <154517645+Kim-Yukyung@users.noreply.github.com>
2024-08-09 11:29:45 +0900

    Add files via upload

---
+++ server.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, BackgroundTasks
+from fastapi.responses import JSONResponse
+from typing import Dict
+import requests
+
+from pydantic import BaseModel
+
+# source ./venv/Scripts/activate
+# uvicorn server:app --reload
+
+# http://127.0.0.1:8000/docs
+
+app = FastAPI()
+
+class Info(BaseModel):
+    accident_type: int
+    latitude: float
+    longitude: float
+
+@app.post("/accident/")
+async def accident_post(data: Info):
+    return data
+    
